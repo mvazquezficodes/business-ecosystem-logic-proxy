c6a7bc3a90bdd57fd37b98b86931b7dc81927fc5
mvazquezficodes <mvazquez@ficodes.com>
2023-11-08 11:47:56 +0100

    changes to account validation

--- controllers/tmf-apis/account.js
+++ controllers/tmf-apis/account.js
@@ -30,21 +30,32 @@ const path = require('path')
 
 const account = (function() {
 
+    var OWNER_ROLE = config.billingAccountOwnerRole;
+
     var getAccountPath = function(asset) {
         return url.parse(asset.account.href).pathname;
     };
 
     var getAccountAPIUrl = function(path) {
+        const accountPath = path.replace(`/${config.endpoints.billing.path}/`, '')
+
         return utils.getAPIURL(
-            config.endpoints.account.appSsl,
-            config.endpoints.account.host,
-            config.endpoints.account.port,
-            path
+            config.endpoints.billing.appSsl,
+            config.endpoints.billing.host,
+            config.endpoints.billing.port,
+            accountPath
         );
     };
 
+
     const retrieveAsset = function(path, callback) {
         const uri = getAccountAPIUrl(path);
+
+        console.log("RetrieveAsset");
+        console.log(path);
+
+        console.log(uri.split('/'));
+
 
         axios.get(uri).then((response) => {
             if (response.status >= 400) {
@@ -59,10 +70,11 @@ const account = (function() {
             }
         }).catch((err) => {
             callback({
-                status: 500
+                status: err.response.status
             });
         })
     };
+
 
     var validateRetrieval = function(req, callback) {
         // Check if the request is a list of resources specifications
@@ -76,72 +88,65 @@ const account = (function() {
     };
 
     var validateRelatedParty = function(req, callback) {
-        var relatedPartyField = "relatedParty";
-        console. log ("ValidateRelatedParty");
+        var relatedPartyField = 'relatedParty';
+
         // Creation request must include relatedParty field. Otherwise, an error must be arisen
-        if (req.method === "POST" && !(relatedPartyField in req-json)) {
+
+        if (req.method === 'POST' && !(relatedPartyField in req.json)) {
             callback({
                 status: 422,
                 message: 'Billing Accounts cannot be created without related parties'
             });
         } else {
-        // This part only be executed for update requests or for creation requests that include
-        // the relatedParty field.
-            if (!(relatedPartyField in req-json) || tmfUtils.hasPartyRole(req, req-json. relatedParty, OWNER_ROLE)) {
-                callback (null);
+            // This part only be executed for update requests or for creation requests that include
+            // the relatedParty field.
+
+            if (!(relatedPartyField in req.json) || tmfUtils.hasPartyRole(req, req.json.relatedParty, OWNER_ROLE)) {
+                callback(null);
             } else {
                 callback({
-                status: 403,
-                message: "The user making the request and the specified owner are not the same user"
+                    status: 403,
+                    message: 'The user making the request and the specified owner are not the same user'
                 });
             }
         }
     };
 
-    var isOwner = function(req, asset, notAuthorizedMessage, callback) {
-        if ('account' in asset) {
-            // Customer Account - The attached customer need to be checked
-            var accountPath = getAccountPath(asset);
-
-            retrieveAsset(accountPath, function(err, result) {
-                if (err) {
+    var validateOwner = function(req, callback) {
+        retrieveAsset(req.apiUrl, function(err, response) {
+            if (err) {
+                if (err.status === 404) {
                     callback({
-                        status: 500,
-                        message: 'The attached account cannot be retrieved'
+                        status: 404,
+                        message: 'The required billing account does not exist'
                     });
                 } else {
-                    const account = result.body;
-
-                    if (tmfUtils.hasPartyRole(req, [account.relatedParty], 'owner')) {
-                        callback(null);
-                    } else {
-                        callback({
-                            status: 403,
-                            message: notAuthorizedMessage
-                        });
-                    }
+                    callback({
+                        status: 500,
+                        message: 'The required billing account cannot be created/updated'
+                    });
                 }
-            });
-        } else {
-            // Customer - Related party is directly included
-            if (tmfUtils.hasPartyRole(req, [asset.relatedParty], 'owner')) {
-                callback(null);
             } else {
-                callback({
-                    status: 403,
-                    message: notAuthorizedMessage
-                });
+                if (!tmfUtils.hasPartyRole(req, response.body.relatedParty, 'owner') || !utils.hasRole(req.user, config.oauth2.roles.seller)) {
+                    callback({
+                        status: 403,
+                        message: 'Unauthorized to update non-owned/non-seller billing account'
+                    });
+                }        
+                else{
+                    callback(null)
+                }
             }
-        }
+        });
     };
 
     var validators = {
         GET: [utils.validateLoggedIn, validateRetrieval],
         //GET: [utils.validateLoggedIn],
         //POST: [utils.validateLoggedIn, validateCreation, validateAccountAccountNotIncluded],
-        POST: [utils.validateLoggedIn],
+        POST: [utils.validateLoggedIn, validateRelatedParty],
         //PATCH: [utils.validateLoggedIn, validateUpdateOwner, validateIDNotModified, validateAccountAccountNotIncluded],
-        PATCH: [utils.validateLoggedIn],
+        PATCH: [utils.validateLoggedIn, validateRelatedParty/*, validateOwner*/],
         // This method is not implemented by this API
         //'PUT': [ utils.validateLoggedIn, validateOwner, validateCreation ],
         DELETE: [utils.validateLoggedIn]
@@ -156,7 +161,15 @@ const account = (function() {
         var regExpResult = pathRegExp.exec(apiPath);
 
         const aux = req.apiUrl;
+        console.log("Print body");
+        console.log(req.body);
         req.apiUrl = '/' + config.endpoints.billing.path + aux;
+
+
+        /*const aux = req.apiUrl;
+        console.log("Print aux");
+        console.log(aux);
+        req.apiUrl = '/' + config.endpoints.billing.path + aux;*/
 
         if (regExpResult) {
             req.isCollection = regExpResult[3] ? false : true;
@@ -220,10 +233,6 @@ const account = (function() {
             return callback(null);
         }
     };
-
-    /*var executePostValidation = function(proxyRes, callback) {
-        callback(null);
-    }*/
     
     return {
         checkPermissions: checkPermissions,
